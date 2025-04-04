9034f59dde0e280fce8ae67a1a6670098cb613e1
Stephen DiCato <stephendicato@gmail.com>
2025-04-04 10:23:12 -0400

    WIP: update ththeme and social icons.

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,14 @@
 AUTHOR = 'Racing with Friends'
 SITENAME = 'Racing with Friends'
-SITEURL = ""
+SITEURL = "https://racingwithfriends.com"
 
 PATH = "content"
 
 TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = 'en'
+
+THEME = 'simple-bootstrap'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -17,16 +19,10 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
 )
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
 )
 
 DEFAULT_PAGINATION = False
